c4d0e4ea8c294171a8cb55bab412bbf90042b7c5
Djezo1993 <djezo83@gmail.com>
2024-06-06 20:23:47 +0200

    Update logo.js

--- logo.js
+++ logo.js
@@ -1,12 +1,1 @@
-document.addEventListener('scroll', function() {
-    const logoDefault = document.querySelector('.logo-default');
-    const logoScroll = document.querySelector('.logo-scroll');
 
-    if (window.scrollY > 50) {
-        logoDefault.style.display = 'none';
-        logoScroll.style.display = 'block';
-    } else {
-        logoDefault.style.display = 'block';
-        logoScroll.style.display = 'none';
-    }
-});
